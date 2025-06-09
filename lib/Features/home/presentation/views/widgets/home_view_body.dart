bc1eca32792d32250b31bfbf3f266248559a9891
Salma Mohamed <132566978+Salmamoh22@users.noreply.github.com>
2025-06-09 23:09:08 +0200

    create styles for app

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Core/utils/styles.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -16,8 +17,9 @@ class HomeViewBody extends StatelessWidget {
         SizedBox(height: 50),
         Padding(
           padding: EdgeInsets.only(left: 24),
-          child: Text('Best Seller', style: Styles.titleMedium),
+          child: Text('Best Seller', style: Styles.textStyle18),
         ),
+        BestSellerListViewItem(),
       ],
     );
   }

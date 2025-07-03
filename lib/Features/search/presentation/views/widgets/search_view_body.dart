810b799c6014e50d7fc4cef3e5ed88ddfe433d75
Salma Mohamed <132566978+Salmamoh22@users.noreply.github.com>
2025-07-03 15:21:36 +0200

    search view done

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/Core/utils/styles.dart';
 import 'package:bookly_app/Features/search/presentation/views/widgets/custom_text_field.dart';
+import 'package:bookly_app/Features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -8,7 +10,16 @@ class SearchViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [CustomTextField()]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomTextField(),
+          SizedBox(height: 16),
+          Text("Search Result", style: Styles.textStyle18),
+          SizedBox(height: 16),
+          Expanded(child: SearchResultListView()),
+        ],
+      ),
     );
   }
 }

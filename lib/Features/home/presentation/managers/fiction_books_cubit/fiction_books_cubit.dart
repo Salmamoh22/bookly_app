2c003611d99330bf5b1b47d2dec30368d6b761e0
Salma Mohamed <132566978+Salmamoh22@users.noreply.github.com>
2025-07-05 22:47:50 +0200

    Fiction Books Cubit Done

--- lib/Features/home/presentation/managers/fiction_books_cubit/fiction_books_cubit.dart
+++ lib/Features/home/presentation/managers/fiction_books_cubit/fiction_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'fiction_books_state.dart';
 
 class FictionBooksCubit extends Cubit<FictionBooksState> {
-  FictionBooksCubit() : super(FictionBooksInitial());
+  FictionBooksCubit(this.homeRepo) : super(FictionBooksInitial());
+  final HomeRepo homeRepo;
+
+    Future<void> fetchFictionBooks() async {
+    emit(FictionBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(FictionBooksFailure(failure.errorMessage)),
+      (books) => emit(FictionBooksSuccess(books)),
+    );
+  }
 }
